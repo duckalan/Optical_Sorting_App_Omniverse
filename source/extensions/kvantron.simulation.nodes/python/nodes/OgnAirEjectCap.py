dab63e8fa0c3667da074071361f2163f455e47cd
duck_alan <duckalan2@gmail.com>
2025-11-29 22:48:23 +0300

    Add full UI control for AirEjectCap

--- source/extensions/kvantron.simulation.nodes/python/nodes/OgnAirEjectCap.py
+++ source/extensions/kvantron.simulation.nodes/python/nodes/OgnAirEjectCap.py
@@ -9,22 +9,23 @@ class OgnAirEjectCap:
 
     @staticmethod
     def compute(db: OgnAirEjectCapDatabase):
-        origin = db.inputs.overlapOrigin #carb.Float3(0.0, 0.0, 50.0)
+        origin = db.inputs.overlapOrigin
         boxSize = db.inputs.boxSize
         extent = carb.Float3(boxSize / 2, boxSize / 2, boxSize / 2)
         rotation = carb.Float4(0.0, 0.0, 0.0, 1.0)
+        impulse = carb.Float3(*db.inputs.impulse)
+        forceOrigin = carb.Float3(*db.inputs.forceOrigin)
         last_rigid_body = None
 
         def overlap_callback(hit: omni.physx.bindings._physx.OverlapHit):
             nonlocal last_rigid_body
             if last_rigid_body != hit.rigid_body_encoded[0]:
-                # carb.log_warn(f"OverlapPrim: {hit.rigid_body}, {hit.rigid_body_encoded}")
                 physx_simulation = omni.physx.get_physx_simulation_interface()
                 physx_simulation.apply_force_at_pos(
                     physx_simulation.get_attached_stage(),
                     hit.rigid_body_encoded[0],
-                    carb.Float3(0, -0.25, 0.1),
-                    carb.Float3(0.0, 0.0, 0.0),
+                    impulse,
+                    forceOrigin,
                     mode='Impulse'
                 )
                 last_rigid_body = hit.rigid_body_encoded[0]
@@ -32,13 +33,11 @@ class OgnAirEjectCap:
             return True
 
         physx_scene_query = omni.physx.get_physx_scene_query_interface()
-        temp = physx_scene_query.overlap_box(
+        physx_scene_query.overlap_box(
             extent,
             origin,
             rotation,
             overlap_callback
         )
-        # carb.log_warn(f"Overlaped: {temp}")
-
 
         return True
